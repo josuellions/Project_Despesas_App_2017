ed422f61124d95e9b7dfb19bf05524e281996230
josuellions <josuel_lions@hotmail.com>
2018-04-12 01:33:23 -0300

    implementando AngularJS e refatorando code

--- controller_angularjs.js
+++ controller_angularjs.js
@@ -3,19 +3,19 @@ app.config(function ($routeProvider) {
   $routeProvider
     .when('/', {
       //template: 'Hello World - Welcom user!',
-      templateUrl: '/public/home.html'
+      templateUrl: '/android_asset/www/public/home.html'
     })
     .when('/despesas', {
-      templateUrl: '/public/despesas.html',
+      templateUrl: '/android_asset/www/public/despesas.html',
 
       //templeteUrl: '/public/menuMobile.html'
     })
     .when('/entrada', {
-      templateUrl: '/public/entrada.html',
+      templateUrl: '/android_asset/www/public/entrada.html',
       //templeteView: '/public/menuMobile.html'
     })
     .when('/visualizar', {
-      templateUrl: '/public/visualiza.html'
+      templateUrl: '/android_asset/www/public/visualiza.html'
     })
     .when('/relatorio', {
       //templateUrl: '/public/relariorio.html',
@@ -28,11 +28,12 @@ app.config(function ($routeProvider) {
 //console teste=> window.location.hast = '#/index';
 
 app.controller('navegarApp', function ($scope, $location) {
-  
+console.log($location.path);  
   $scope.submit = function ($page) {
     if ($page == 'entrada') {
       //window.location.hash = '#/teste';
       $location.path('/entrada');
+
       document.location.reload(true);
     } else if ($page == 'despesas') {
       $location.path('/despesas');
