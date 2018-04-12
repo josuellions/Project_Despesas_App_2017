3d09491b0031828b236a784f3c721cc3cf3e98bf
josuellions <josuel_lions@hotmail.com>
2018-04-12 02:20:40 -0300

    implementando AngularJS e refatorando code

--- controller_angularjs.js
+++ controller_angularjs.js
@@ -2,33 +2,24 @@ var app = angular.module('todoApp', ['ngRoute']);
 app.config(function ($routeProvider) {
   $routeProvider
     .when('/', {
-      //template: 'Hello World - Welcom user!',
       templateUrl: '/android_asset/www/public/home.html'
     })
     .when('/despesas', {
       templateUrl: '/android_asset/www/public/despesas.html',
 
-      //templeteUrl: '/public/menuMobile.html'
     })
     .when('/entrada', {
       templateUrl: '/android_asset/www/public/entrada.html',
-      //templeteView: '/public/menuMobile.html'
     })
     .when('/visualizar', {
       templateUrl: '/android_asset/www/public/visualiza.html'
-    })
-    .when('/relatorio', {
-      //templateUrl: '/public/relariorio.html',
-
     })
     .otherwise({
       redirectTo: '/'
     });
 });
-//console teste=> window.location.hast = '#/index';
 
 app.controller('navegarApp', function ($scope, $location) {
-console.log($location.path);  
   $scope.submit = function ($page) {
     if ($page == 'entrada') {
       //window.location.hash = '#/teste';
