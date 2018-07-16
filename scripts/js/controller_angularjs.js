1081128a2d50abc650c53f1afb660db469786fb6
josuellions <josuel_lions@hotmail.com>
2018-07-16 04:40:16 -0300

    checked despesas pagas refatorando code e layout note8

--- scripts/js/controller_angularjs.js
+++ scripts/js/controller_angularjs.js
@@ -11,7 +11,7 @@ app.config(function ($routeProvider) {
       templateUrl: '/android_asset/www/public/home.html'
     })
     .when('/despesas', {
-      templateUrl: url + 'public/despesas.html',
+      //templateUrl: url + 'public/despesas.html',
       templateUrl: '/android_asset/www/public/despesas.html',
       //templeteUrl: '/public/menuMobile.html'
     })
