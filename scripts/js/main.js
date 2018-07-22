fbe3a3cf71554b86b5f18632e6680f6de75c2c44
josuellions <josuel_lions@hotmail.com>
2018-07-22 03:03:15 -0300

    remodelando layout view, refatorando com angulajs

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -13,22 +13,22 @@ angular.module('todoApp', ['ngRoute', 'depesasDirectives'])
       //template: 'Hello World - Welcom user!',
       //templateUrl: '/public/home.html'
       //templateUrl: url + 'public/home.html'
-      templateUrl: 'home.html',
+      templateUrl: '/android_asset/www/home.html',
      // controller: 'DespesasController'
     })
     .when('/despesas', {
       //templateUrl: url + 'public/despesas.html',
-      templateUrl: 'despesas.html'
+      templateUrl: '/android_asset/www/despesas.html'
 
       //templeteUrl: '/public/menuMobile.html'
     })
     .when('/entrada', {
-         templateUrl: 'entrada.html'
+      templateUrl: '/android_asset/www/entrada.html'
          //templateUrl: url + 'public/entrada.html',
          //templeteView: '/public/menuMobile.html'
     })
     .when('/visualizar', {
-      templateUrl: 'visualiza.html'
+      templateUrl: '/android_asset/www/visualiza.html'
       //templateUrl: '/android_asset/www/public/visualiza.html'
     })
     .when('/relatorio', {
