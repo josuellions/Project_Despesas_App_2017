61f47302d3f0dabc1664c89fb394f51b29be0566
josuellions <josuel_lions@hotmail.com>
2018-09-11 20:40:50 -0300

    AngulasJs, insert dados mês automatizado, view relatorio, informações, sobre

--- scripts/js/main.js
+++ scripts/js/main.js
@@ -31,15 +31,15 @@ angular.module('todoApp', ['ngRoute', 'ngAnimate', 'ngResource', 'depesasDirecti
         })
         .when('/relatorio', {
           templateUrl: '/android_asset/www/public/relatorio.html',
-          controller: 'NavegarController'
+          //controller: 'NavegarController'
         })
         .when('/informacoes', {
           templateUrl: '/android_asset/www/public/informacoes.html',
-          controller: 'NavegarController'
+          //controller: 'NavegarController'
         })
         .when('/sobre', {
           templateUrl: '/android_asset/www/public/sobre.html',
-          controller: 'NavegarController'
+          //controller: 'NavegarController'
         })
         .otherwise({
           redirectTo: '/'
@@ -67,15 +67,15 @@ angular.module('todoApp', ['ngRoute', 'ngAnimate', 'ngResource', 'depesasDirecti
         })
         .when('/relatorio', {
           templateUrl: '/public/relatorio.html',
-          controller: 'NavegarController'
+          //controller: 'NavegarController'
         })
         .when('/informacoes', {
           templateUrl: '/public/informacoes.html',
-          controller: 'NavegarController'
+          //controller: 'NavegarController'
         })
         .when('/sobre', {
           templateUrl: '/public/sobre.html',
-          controller: 'NavegarController'
+          //controller: 'NavegarController'
         })
         .otherwise({
           redirectTo: '/',
