0643cde4d459f6cd446e3bb42b72299e4e9f575c
josuel A. Lopes <josuellions@gmail.com>
2021-01-11 22:03:03 -0300

    AngularJS - Check connection Bkp server - versao v1.0.7c select server  connection

--- scripts/js/controllers/backup-controller.js
+++ scripts/js/controllers/backup-controller.js
@@ -3,13 +3,53 @@ angular
   .controller("BackupController", function ($http, $location) {
     
     let totalTable;
-    const urlbase = 'http://21.21.21.4:3333';
+    let urlbase = ''; //'http://21.21.21.11:3333';
+
+    const SetUrlServer = () =>{
+      const enderecoServer = prompt("Digite endereço ip ou url do servido.") //'21.21.21.11'; //
+
+      if(String(enderecoServer).length > 0){
+        urlbase = `http://${enderecoServer}:3333`;
+        return;
+      }
+
+      SetUrlServer();
+    }
+
+    SetUrlServer();
 
     function Connection () {
+/*
       const conect = () => {   
         return $http.get(`${urlbase}/connection`);
       }
+      */
+      const conect = () => {   
+        $.ajax({
+          url: `${urlbase}/connection`,
+          type: "GET",
+          crossDomain: true,
+          //data: JSON.stringify(somejson),
+          dataType: "json",
+          success: function (response,status) {
+              //var resp = JSON.parse(response)
+              //console.log(response, status);
+              if(status == 'success'){
+                ResponseConnection(true); 
+                //console.log(res.data)
+              }
+          },
+          error: function (xhr, status) {
+              //alert("error");
+              //console.log(status)
+              console.error("Falha na conexão server!");
+              ResponseConnection(false); 
+          }
+        });
+      }
 
+      conect();
+      /*
       conect().then((res)=>{
         if(res.status == 201){
           ResponseConnection(true); 
@@ -20,6 +60,7 @@ angular
         console.error("Falha na conexão server!");
         ResponseConnection(false);         
       })
+      */
     }
 
     const ResponseConnection = (conn) => {
@@ -31,21 +72,81 @@ angular
       }
       const urlEntrada = `${urlbase}/entrada`;
 
+      async function EnviarDados (data) { 
+        
+      }
+
       const Components = {
         TbDespesas(data) { 
-          return $http.post(urlEntrada, {
+          /*return $http.post(urlEntrada, {
             Despesas: data
+          });*/
+
+          $.ajax({
+            url: `${urlEntrada}`,
+            type:  'POST',
+            crossDomain: true,
+            contentType: 'application/json',
+            data: JSON.stringify(data).replace('0','Despesas'),
+            dataType: "json",
+            success: function (response,status) {
+                if(status == 'success'){
+                  --totalTable;
+                }
+            },
+            error: function (xhr, status) {
+                console.error("Falha na conexão server!");
+                ResponseConnection(false); 
+            }
           });
         },
         TbDespesasStatus(data) {
-          return $http.post(urlEntrada, {
+          /*return $http.post(urlEntrada, {
             DespesasStatus: data
           })
+          */
+
+          $.ajax({
+            url: `${urlEntrada}`,
+            type:  'POST',
+            crossDomain: true,
+            contentType: 'application/json',
+            data: JSON.stringify(data).replace('0','DespesasStatus'),
+            dataType: "json",
+            success: function (response,status) {
+                if(status == 'success'){
+                  --totalTable;
+                }
+            },
+            error: function (xhr, status) {
+                console.error("Falha na conexão server!");
+                ResponseConnection(false); 
+            }
+          });
         },
         TbEntradas(data) {
-          return $http.post(urlEntrada, {
+          /*return $http.post(urlEntrada, {
             Entrada: data
           })
+          */
+
+          $.ajax({
+            url: `${urlEntrada}`,
+            type:  'POST',
+            crossDomain: true,
+            contentType: 'application/json',
+            data: JSON.stringify(data).replace('0','Entrada'),
+            dataType: "json",
+            success: function (response,status) {
+                if(status == 'success'){
+                  --totalTable;
+                }
+            },
+            error: function (xhr, status) {
+                console.error("Falha na conexão server!");
+                ResponseConnection(false); 
+            }
+          });
         }
       }
   
@@ -96,14 +197,13 @@ angular
         }
       };
           
+      /*  
       const enviar = (table, data) => {
-  
         const action = Components[table]
-  
-       return action(data);
+
+        return action(data);
   
       }
-  
       async function PostData (table, data) {
        await enviar(table, data).then((res)=>{
          if(res.status == 201){
@@ -116,7 +216,19 @@ angular
          console.error("Falha na comunicação server!")
        })
       }
-  
+  */
+
+    //async
+     function PostData (table, data) {
+      //await 
+      //enviar(table, data)
+
+      const action = Components[table]
+
+      return action(data);
+        
+    }
+
     PercorrerBancoDadosParaBackup(true)
 
     }
